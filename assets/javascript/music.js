6da79349d32f46bd49ad5ccf4e92e6804e6291bd
URxFAVxAUTHOR <nygel.porterfield@gmail.com>
2019-02-16 19:31:07 -0500

    initial push

---
+++ assets/javascript/music.js
@@ -0,0 +1,13 @@
+$('document').ready() 
+
+$("#test").click(function() {  
+    console.log("Hello");
+    alert("Hello!");
+    });
+
+    $('#testButton').click(function() {
+        console.log("hello");
+        alert("Hello again!");
+    })
+
+
